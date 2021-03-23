c1849fe746fe7ca1d19a0fee4e63513ea015314a
sudheerbolla <sudheerbolla@webilize.com>
2021-03-23 07:19:40 -0700

    removed unnecessary logs and checkout issue done

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -1,9 +1,7 @@
 const data = require('../data/data');
 
 exports.checkout = function(req, res)  {
-    const sku = req.body.sku;
-    console.log("Received add parameter. SKU: " + sku);
-    data.cart = data.cart.clear();
+    data.cart =[];
     res.header("Content-Type: application/json");
     res.send({"inventory" : data.inventory, "cart": data.cart}); 
 }
--- controllers/removeItemBns35Controller.js
+++ controllers/removeItemBns35Controller.js
@@ -3,7 +3,6 @@ const utilClass = require('../util/UtilClass');
 
 exports.deleteItemFromCart = function(req, res)  {
     const sku = req.body.sku;
-    console.log("Received delete parameter. SKU: " + sku);
     data.cart = utilClass.editList(data.cart, '-', sku, false);
     data.inventory = utilClass.editList(data.inventory, '+', sku, true);
     res.header("Content-Type: application/json");
